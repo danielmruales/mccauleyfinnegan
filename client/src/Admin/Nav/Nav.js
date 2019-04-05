5c580a9bf2d8cd71b84a305b85a8d58ac0a45f6e
Daniel Ruales <danielmruales@gmail.com>
2019-04-05 17:04:10 -0600

    server setup

--- client/src/Admin/Nav/Nav.js
+++ client/src/Admin/Nav/Nav.js
@@ -6,6 +6,7 @@ const Nav = () => {
     return (
         <div className='adminNav'>
             <Link to='/' className='adminItems'>Home</Link>
+            <Link to='/admin' className='adminItems'>Admin</Link>
             <Link to='/submissons'className='adminItems'>Contact Submissions</Link>
             <Link to='/pictures' className='adminItems'>Pictures</Link>
         </div>
