6eb3e5f873dfc2bc471716bfd6c050e2b22a5b40
Daniel Ruales <danielmruales@gmail.com>
2019-04-07 21:38:13 -0600

    finished home page, waiting for content for it but everything else is done.

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar.js'
+import {Link} from 'react-router-dom'
 import './Home.css'
 
 const Home = () => {
@@ -7,20 +8,25 @@ const Home = () => {
         <div>
             <Navbar/>
             <div className='heroPic'>
-                <h1>Mccauley Finnegan</h1>
+                <div className='heroTitleDiv'><h1 className='heroTitle'>McCauley Finnegan</h1></div>
             </div>
             <div className='secondSection'> 
-                <h1 className='secondTitle'>Build Your Brand with MC FINNY</h1>
+                <h1 className='secondTitle'>Words to tell them what you offer/why you</h1>
+                <hr width="1" size="100" color='#F7A9A9'/>
+                {/* <div className='verticalLine'></div> */}
                 <h2 className='servicesTitle'>Services</h2>
                 <div className='homePics'>
-                    <img src='http://www.dealforsoftware.com/wp-content/uploads/2018/09/6198521760_aa86027669_z.jpg' alt='services pictures' className='gridPics'/>
-                    <img src='http://www.dealforsoftware.com/wp-content/uploads/2018/09/6198521760_aa86027669_z.jpg' alt='services pictures' className='gridPics'/>
-                    <img src='http://www.dealforsoftware.com/wp-content/uploads/2018/09/6198521760_aa86027669_z.jpg' alt='services pictures' className='gridPics'/>
-                    <img src='http://www.dealforsoftware.com/wp-content/uploads/2018/09/6198521760_aa86027669_z.jpg' alt='services pictures' className='gridPics'/>
+                    <img src='https://www.mcfinneganphotography.com/uploads/9/1/6/0/91608040/editor/portraits.png?1519180663' alt='services pictures' className='topLeft' onClick={<Link to='/services'/>}/> 
+                    <img src='https://www.mcfinneganphotography.com/uploads/9/1/6/0/91608040/editor/food.png?1519180934' alt='services pictures' className='topRight'/>
+                    <img src='https://www.mcfinneganphotography.com/uploads/9/1/6/0/91608040/editor/product.png?1519181385' alt='services pictures' className='bottomLeft'/>
+                    <img src='https://www.mcfinneganphotography.com/uploads/9/1/6/0/91608040/editor/travel.png?1519181588' alt='services pictures' className='bottomRight'/>
                 </div>
+                <hr width="1" size="100" color='#F7A9A9'/>
+                <h1 className='thirdTitle'>Words to make them Like you</h1>
+                <Link to='/contact' className='contactBtn'>Get In Touch </Link>
             </div>
         </div>
     );
 };
 
-export default Home;
+export default Home; 
