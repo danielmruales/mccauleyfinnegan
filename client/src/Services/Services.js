a6ab764a4b98bbdd1a6b5b21f32c895c5adb0fbc
Daniel Ruales <danielmruales@gmail.com>
2019-04-12 17:52:02 -0600

    changed title and added to services page layout

--- client/src/Services/Services.js
+++ client/src/Services/Services.js
@@ -7,9 +7,14 @@ class Services extends Component {
         return (
             <div>
                 <Navbar/>
-                <div>
-                    <h1 className='serviceTitle'>Grow Your Brand With MC Finny</h1>
+                <h1>Grow Your Brand</h1>
+                <div className='serviceTest'>
+                    <img src='https://www.mcfinneganphotography.com/uploads/9/1/6/0/91608040/img-3463_orig.jpg'alt='' className='marketingPic'/>
+                    <img src='https://www.mcfinneganphotography.com/uploads/9/1/6/0/91608040/img-2179_2_orig.jpg'alt='' className='photoPic'/>
+                    <img src='https://www.mcfinneganphotography.com/uploads/9/1/6/0/91608040/img-0984_1_orig.jpg'alt='' className='businessPic'/>
                 </div>
+                <h1 className='serviceTitle'>Grow Your Brand With MC Finny</h1>
+                
                 <div className='marketing'>
                     <img src='https://www.mcfinneganphotography.com/uploads/9/1/6/0/91608040/img-9174_orig.jpg'alt='' className='marketingPic'/>
                     <h1 className='marketingTitle'>Marketing</h1>
@@ -20,7 +25,7 @@ class Services extends Component {
                 </div>
                 <div className='business'>
                     <img src='https://www.mcfinneganphotography.com/uploads/9/1/6/0/91608040/img-0860-2_3_orig.jpg'alt='' className='businessPic'/>
-                    <h1 className='businessTitle'>Business Consulting</h1>
+                    <h1 className='businessTitle'> Business Consulting</h1>
                 </div>
             </div>
         );
@@ -28,3 +33,4 @@ class Services extends Component {
 }
 
 export default Services;
+
