ad553c1acc1ebbd5f1626bf8d3a80f2ef04de0bd
Daniel Ruales <danielmruales@gmail.com>
2019-04-17 17:03:29 -0600

    changed layout of pics on home page

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -13,13 +13,13 @@ const Home = (props) => {
             <div className='secondSection'> 
                 <h1 className='secondTitle'>Words to tell them what you offer/why you</h1>
                 <hr width="1" size="100" color='#fff' style={{borderLeft: 'dashed 2pt #F7A9A9'}}/>
-                {/* <div className='verticalLine'></div> */}
                 <h2 className='servicesTitle'>Services</h2>
                 <div className='homePics'>
                     <img src='https://www.mcfinneganphotography.com/uploads/9/1/6/0/91608040/editor/portraits.png?1519180663' alt='services pictures' className='topLeft' onClick={() => props.history.push('/services')}/> 
-                    <img src='https://www.mcfinneganphotography.com/uploads/9/1/6/0/91608040/editor/food.png?1519180934' alt='services pictures' className='topRight'/>
-                    <img src='https://www.mcfinneganphotography.com/uploads/9/1/6/0/91608040/editor/product.png?1519181385' alt='services pictures' className='bottomLeft'/>
-                    <img src='https://www.mcfinneganphotography.com/uploads/9/1/6/0/91608040/editor/travel.png?1519181588' alt='services pictures' className='bottomRight'/>
+                    <img src='https://www.mcfinneganphotography.com/uploads/9/1/6/0/91608040/editor/food.png?1519180934' alt='services pictures' className='topRight' onClick={() => props.history.push('/services')} />
+                </div>
+                <div className='thirdImageDiv'> 
+                    <img src='https://www.mcfinneganphotography.com/uploads/9/1/6/0/91608040/editor/product.png?1519181385' alt='services pictures' className='bottomLeft' onClick={() => props.history.push('/services')} />
                 </div>
                 <hr width="1" size="100" color='#fff' style={{borderLeft: 'dashed 2pt #F7A9A9'}}/>
                 <h1 className='thirdTitle'>Words to make them Like you</h1>
