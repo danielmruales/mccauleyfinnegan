383c47e9625f6f07a0dc7f984d86c2014cc6fcef
Daniel Ruales <danielmruales@gmail.com>
2019-04-17 00:43:07 -0600

    setup about page and added some media queries

--- client/src/About/About.js
+++ client/src/About/About.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar.js';
-
+import './About.css'
 
 const About = () => {
     return (
         <div>
             <Navbar/>
            <div className='mainAbout'>
-               <img/>
-               <div>
-                   <h1>
-                       MCCAULEY FIN
-                   </h1>
+               <img src='https://images.unsplash.com/photo-1553531384-397c80973a0b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60' alt='mccualey'/>
+               <div className='secondAbout'>
+                   <h1>MCCAULEY FIN</h1>
+                   <p>Lorem Impsum Lorem Impsum Lorem Impsum Lorem Impsum Lorem Impsum</p>
+                   <p>Lorem Impsum Lorem Impsum Lorem Impsum Lorem Impsum Lorem Impsum</p>
+                   <p>Lorem Impsum Lorem Impsum Lorem Impsum Lorem Impsum Lorem Impsum</p>
+                   <p>Lorem Impsum Lorem Impsum Lorem Impsum Lorem Impsum Lorem Impsum</p>
+                   <p>Lorem Impsum Lorem Impsum Lorem Impsum Lorem Impsum Lorem Impsum</p>
                </div>
            </div>
         </div>
